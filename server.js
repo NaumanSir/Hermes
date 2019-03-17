3f0c51bcdeba377de352fd9436cb4db8c6e641bf
Nauman Husnani <naumanhusnani@gmail.com>
2019-03-17 14:33:15 -0500

    runs on port 3000, but not 4000 for some reason

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(6000, function() {
-    console.log("Listening on port 6000.")
+var server = app.listen(3000, function() {
+    console.log("Listening on port 3000.")
 });
 
 app.use(express.static('public'));
