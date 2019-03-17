2b5ccacf29d0d3e4bc6f06657951d4bda585d3c9
Nauman Husnani <naumanhusnani@gmail.com>
2019-03-17 14:42:34 -0500

    added socket.io to the backend

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var socket = require('socket.io');
 
+// Express setup
 var app = express();
 var server = app.listen(3000, function() {
     console.log("Listening on port 3000.")
 });
 
 app.use(express.static('public'));
+
+// Socket setup
+var io = socket(server);
