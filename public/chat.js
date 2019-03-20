7723cf1501b368e6c7edb4f35c048e1ba281c141
Nauman Husnani <naumanhusnani@gmail.com>
2019-03-20 11:46:25 -0500

    chat working - found error in chat.js

--- public/chat.js
+++ public/chat.js
@@ -2,9 +2,9 @@ var socket = io.connect("http://localhost:3000");
 
 
 // Query DOM
-var message = document.getElementById('message');
-    handle = document.getElementById('handle');
-    send = document.getElementById('send');
+var message = document.getElementById('message'),
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
     output = document.getElementById('output');
 
 // Emitting events
