7df2337613108ae32b49a12093d5ac7d8d41df44
Nauman Husnani <naumanhusnani@gmail.com>
2019-03-21 19:30:25 -0500

    broadcast works!

--- public/chat.js
+++ public/chat.js
@@ -16,8 +16,16 @@ btn.addEventListener('click', function(){
     })
 });
 
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
+});
+
 // Listening for events
 socket.on('chat', function(data){
     output.innerHTML += '<p><strong>' + data.handle +
     ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message . . . </em></p>';
 });
